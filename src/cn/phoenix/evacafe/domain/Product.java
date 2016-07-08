50bb2ca2aa422026a53c77385d26b6761597a4a2
= <=>
2016-07-08 11:35:11 +0800

    1

---
+++ src/cn/phoenix/evacafe/domain/Product.java
@@ -0,0 +1,14 @@
+package cn.phoenix.evacafe.domain;
+
+/**
+ * Created by Phoenix on 2016/7/8.
+ */
+public class Product {
+    //商品编号
+    private int Product_id;
+    /**
+     * 商品种类
+     *
+     * 咖啡粉、咖啡豆、挂耳咖啡、
+     */
+}
